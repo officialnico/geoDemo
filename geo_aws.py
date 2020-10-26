fd6fda2c494f9f32b018d69d7eed6f26c801db73
officialnico <24nico@gmail.com>
2020-10-26 18:01:51 -0400

    demo completed

---
+++ geo_aws.py
@@ -0,0 +1,17 @@
+import ccxt
+import time
+import os
+
+#Code on Tokyo server
+binance = ccxt.binance({'apiKey':'JIgMetKmXC4ce9fnzbCznNfML7KN5SXccnxFi69JP7CnCKHUAQAOpOo4M8etyIBL','secret':'ieD0IvEtAzhHYA6ncgCce5Wog6RSFaNt8KqrLWuMGux8rClaPMsua5ELVACRjHWj'})
+t1 = time.time()
+balance = binance.fetchBalance()
+speed_here = time.time()-t1
+
+cmd = """ssh -i 'tokyo_test.pem' ec2-user@ec2-3-112-93-225.ap-northeast-1.compute.amazonaws.com python3.8 speed_test.py"""
+speed_tokyo = float(os.popen(cmd).read())
+
+print("""request type: fetchBalance""")
+print("current location:", speed_here)
+print("server at AWS location of exchange:", speed_tokyo)
+print("request time difference:", int((speed_here-speed_tokyo)/(speed_tokyo)*100),'%')
